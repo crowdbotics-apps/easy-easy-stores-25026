65fe273d93672ae420fc29581a8566b2994edcee
Crowdbotics <team@crowdbotics.com>
2021-03-14 22:44:51 +0000

    Update models

--- backend/easy_easy_stores_25026/wsgi.py
+++ backend/easy_easy_stores_25026/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'easy_easy_stores_25026.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "easy_easy_stores_25026.settings")
 
 application = get_wsgi_application()
